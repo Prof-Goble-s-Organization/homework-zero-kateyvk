7dbaa00f3d1974cde0ab6fe66316e4e969a7f330
kateyvk <kateyvk16@gmail.com>
2024-09-06 11:54:45 -0400

    question 1 work

--- src/hw0/SpaceAlien2.java
+++ src/hw0/SpaceAlien2.java
@@ -2,36 +2,34 @@ package hw0;
 
 public class SpaceAlien2 extends SpaceAlien implements ZapsWithSlime {
 
-    private String msg;
-
-
     public SpaceAlien2(String homePlanet, double distanceFromEarth) {
         super(homePlanet, distanceFromEarth);
 
     }
     public void doGreeting(String msg){
-        this.msg = msg;
         System.out.println(msg);
 
     }
+    @Override 
     public String doSlimeZap (int numHumans){
 
         if (numHumans >= 1){
-            System.out.println("You slimed " + numHumans + " Humans.");
+            System.out.println("You slimed " + numHumans + " Humans!");
 
             return "blech";
 
 
-        }else{
+        }else if(numHumans<=0){
             return "No humans slimed";
+        }else{
+            return "error";
         }
         
 
 
     }
+    @Override
     public String slimeWholeEarth (){
-        System.out.println("YOU SLIMED THE ENTIRE EARTH!!");
-
         return "BLECHHHHHH";
 
     }
@@ -39,6 +37,8 @@ public class SpaceAlien2 extends SpaceAlien implements ZapsWithSlime {
     public static void main(String[] args) {
         SpaceAlien2 bob = new SpaceAlien2("Jupiter", 42);
         bob.doGreeting("Hi I am bob");
+        bob.slimeWholeEarth();
+
     }
 
 
